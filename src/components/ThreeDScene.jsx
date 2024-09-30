8e36e481390501bb96c0ee2679354998c6667f60
ivaniuss <ivan.emi94@gmail.com>
2024-09-29 23:54:16 -0500

    structures

--- src/components/ThreeDScene.jsx
+++ src/components/ThreeDScene.jsx
@@ -1,15 +1,11 @@
-import { Suspense, useRef } from "react";
+import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Environment,
-  CameraControls,
   PerspectiveCamera,
   OrbitControls,
 } from "@react-three/drei";
-import * as THREE from "three";
-import { Room } from "./Room";
 
-// import { Level, Sudo, Camera, Cactus, Box } from './Scene'
 import { Level } from "./Scene";
 
 export const ThreeDScene = ({ scenes, currentSceneIndex }) => {
@@ -35,20 +31,3 @@ export const ThreeDScene = ({ scenes, currentSceneIndex }) => {
     </div>
   );
 };
-
-// <Canvas flat>
-//       <Suspense fallback={null}>
-//         <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.6} />
-//         <ambientLight intensity={Math.PI / 2} />
-//         <mesh scale={4} position={[-1, -4, 7]} rotation={[0, -0.95, 0]}>
-//         <Level modelPath = {scenes[currentSceneIndex].modelPath} />
-//         <meshStandardMaterial color="lightgray" side={THREE.BackSide} />
-//         </mesh>
-//         <Environment preset="city" background blur={1} />
-//         <PerspectiveCamera
-//           makeDefault
-//           position={[0, 0, 5]}
-//           fov={20}
-//         />
-//       </Suspense>
-//     </Canvas>
