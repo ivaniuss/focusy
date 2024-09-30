97f6342c5b2af99a7109487d5732fe2aab336f96
ivaniuss <ivan.emi94@gmail.com>
2024-09-30 10:37:14 -0500

    structures

--- src/components/ThreeDScene.jsx
+++ src/components/ThreeDScene.jsx
@@ -1,24 +1,28 @@
-import { CameraControls, Environment, Gltf } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+"use client";
+import { Canvas } from "@react-three/fiber";
+import { Environment, CameraControls, Gltf } from "@react-three/drei";
 
 export const ThreeDScene = ({ scenes, currentSceneIndex }) => {
   return (
-    <>
+    <div className="w-full h-screen">
       <Canvas camera={{ position: [0, 0, 0.0001] }}>
         <CameraManager />
         <Environment preset="sunset" />
-        <ambientLight intensity={0.8} color='pink' />
-        <Gltf src={scenes[currentSceneIndex].modelPath} position={[0.2, -1.7, -2]} />
+        <ambientLight intensity={0.8} color="pink" />
+        <Gltf
+          src={scenes[currentSceneIndex].modelPath}
+          position={[0.2, -1.7, -2]}
+        />
       </Canvas>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 const CameraManager = () => {
   return (
-    <CameraControls 
-      minZoom={1} 
-      maxZoom={3} 
+    <CameraControls
+      minZoom={1}
+      maxZoom={3}
       polarRotateSpeed={-0.3}
       azimuthRotateSpeed={-0.3}
       mouseButtons={{
@@ -26,9 +30,9 @@ const CameraManager = () => {
         wheel: 16,
       }}
       touches={{
-        one: 32,
+        one: 31,
         two: 512,
       }}
     />
-  )
-}
+  );
+};
